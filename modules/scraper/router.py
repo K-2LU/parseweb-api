6f4c00dba3f4fe59ee16451d58ec181da79d0837
k2lu <rigansust@gmail.com>
2026-03-26 14:52:39 +0600

    feat: init scraper module and router

--- modules/scraper/router.py
+++ modules/scraper/router.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 scraper_router = APIRouter(prefix='/api/v1', tags=['parseweb'])
 
-@router.get('/')
+@scraper_router.get('/')
 async def root():
     return {"message": "hello world"}
